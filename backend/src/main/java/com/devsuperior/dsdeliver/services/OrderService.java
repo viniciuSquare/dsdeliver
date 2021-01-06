fd6bc100c6da6000f725dcbbd58e039dad8ae6af
Vinicius Quadrado <@main>
2021-01-06 16:24:41 -0300

    PUT /orders/{id}/delivered
    Changing the state of an order

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,6 +49,17 @@ public class OrderService {
 			Product product = productRepository.getOne(p.getId());
 			order.getProducts().add(product);
 		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		
+		order.setStatus(OrderStatus.DELIVIRED);
 		
 		order = repository.save(order);
 		return new OrderDTO(order);

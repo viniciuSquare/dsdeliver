5a31643593c5300ce1de0a7a1b59463a6a963a44
Vinicius Quadrado <@main>
2021-01-06 23:00:49 -0300

    AXIOS fetch to beck-end and products listed layout
    Returned info into formatted ProductsCards components.

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 
 function Orders(){
+    const[products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+
+    }, []);
+
     return(
-        <div>
+        <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
